dfdbea8c94cb8e13b9db34aa0ab420ac10f872cc
Juan Forez <juanks339@gmail.com>
2024-05-19 15:58:52 -0500

    ADD: Directive for errors form

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -10,8 +11,15 @@ export class CustomLabelDirective {
     this.setStyle();
   }
 
+  @Input() set errors( value: ValidationErrors | null | undefined ) {
+    this._errors = value;
+    this.setErrorMessage();
+  }
+
   private htmlEl?: ElementRef<HTMLElement>;
   private _color: string = '#000000';
+
+  private _errors?: ValidationErrors | null;
 
   constructor(
     private el: ElementRef<HTMLElement>
@@ -19,9 +27,38 @@ export class CustomLabelDirective {
     this.htmlEl =  el;
   }
 
-  setStyle() {
+  public setStyle() {
     if ( !this.htmlEl ) return;
     this.htmlEl.nativeElement.style.color = this._color;
+  }
+
+  public setErrorMessage() {
+    if ( !this.htmlEl ) return;
+
+    if ( !this._errors ) {
+      this.htmlEl.nativeElement.innerHTML = 'No hay errores.';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+
+    if(errors.includes('required')) {
+      this.htmlEl.nativeElement.innerHTML = 'Este campo es requerido.';
+      return;
+    } 
+
+    if (errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+
+      this.htmlEl.nativeElement.innerHTML = `Minimo ${current}/${min} caracteres.`;      
+      return;
+    }
+
+    if (errors.includes('email')) {
+      this.htmlEl.nativeElement.innerHTML = 'Este campo debe ser un Email valido.';
+      return;
+    }
   }
 
 }
